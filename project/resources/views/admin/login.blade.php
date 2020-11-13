52de940030af86334f4e4a803a0d8c2852d306ed
k.matsutani <rockin.yamada@gmail.com>
2020-11-13 11:10:43 +0900

    create login form

--- project/resources/views/admin/login.blade.php
+++ project/resources/views/admin/login.blade.php
@@ -5,7 +5,7 @@
         <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
         <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
         <p id="profile-name" class="profile-name-card"></p>
-        <form class="form-signin">
+        <form class="form-signin" method="POST" action="{{ route('admin.login') }}">
             @csrf
             <span id="reauth-email" class="reauth-email"></span>
             <input type="email" name="email" id="inputEmail" class="form-control" placeholder="メールアドレス" value="{{ old('name') }}" required autofocus>
--- project/resources/views/layouts/admin_layouts.blade.php
+++ project/resources/views/layouts/admin_layouts.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Practice Admin</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     <link rel="stylesheet" href="{{ asset('css/login.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -30,6 +30,13 @@
         </div>
     </nav>
     @yield('content')
+    <div class="row ft-copyright pt-2 pb-2" style="padding-left: 25px;">
+		<div class="col-sm-4 text-pp-crt">copyright 2020 All  Rights Reserved</div>
+		<div class="col-sm-4 text-pp-crt-rg">Department of Information Reg No :</div>
+		<div class="col-sm-4 developer">
+			<a href="https://topline-tech.com" target="_blank" class="text-pp-crt">By : <b>T</b>es<b>t</b>.inc</a>
+		</div>
+	</div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
